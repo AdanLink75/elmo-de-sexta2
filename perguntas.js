f34a48503e9c029b7f4c30b0379ce0ea229813e8
AdanLink75 <141869449+AdanLink75@users.noreply.github.com>
2024-10-15 11:44:31 -0300

    asd

---
+++ perguntas.js
@@ -0,0 +1,71 @@
+criaCartao(
+    'matematica',
+    'como se calcula a soma dos termos de uma P.A',
+    'utiliza-se a formula: ((a1+an).q)/2'
+)
+
+criaCartao(
+    'matematica',
+    'quais são todos os numeros do pi existentes?',
+    'dica:começa no 3,14'
+)
+
+criaCartao(
+    'matematica',
+    'se andrew usar 100g de maconha,  29 cigarros(20g) e e 2kg de cocaina, qual efeito daria no andrew? e qual a quantidade que ele usou?',
+)
+criaCartao(
+    'matematica',
+    'se um homem caisse do 13° de cabeça para baixo, quantos ossos iria quebrar e ligamentos iria romper?'
+)
+criaCartao(
+    'historia',
+    'após dos eventos da 2 guerra mundial, como ficou Adolf Hitler?'
+)
+criaCartao(
+    'historia',
+    'em qual ano o Lula(atua precidente do Brasil) roubou do Brasil?'
+)
+criaCartao(
+    'historia',
+    'quem era a esposa de charlie chaplin?'
+
+)
+criaCartao(
+    'histoia',
+    'quem foi Gilberto Freyre, e como ele impactou na vida de getulio vargas?'
+)
+criaCartao(
+    'portugues',
+    'para oque se usa o §?'
+)
+criaCartao(
+    'portugues',
+    'qual o livro que andrew mais gosta'
+)
+criaCartao(
+    'portugues',
+    'site todos os livros, lugares e mulheres que já foram vivenciados por Machado de Assis'
+)
+criaCartao(
+    'portugues',
+    'qual o estilo de literatura que dj azeitona produz?'
+)
+criaCartao(
+    'arte',
+    'em qual episodio de "Valdir" Andrezitos reclamo sobre mendigos comunistas?'
+)
+criaCartao(
+    'arte',
+    'Em qual video do Gemaplys, Vito, fala sua iconica frase sobre japones?'
+)
+criaCartao(
+    'arte',
+    'como se foi montado a primeira bomba de hidrogenio?',
+)
+criaCartao(
+    'arte',
+    'Quantas visualizações o Enaldinho tem no video do caça leão? ',
+    'utiliza-se a formula: ((a1+an).q)/2'
+)
+
