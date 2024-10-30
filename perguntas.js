be9b3606d66d26826781cf01050d4fe5cdc2bbc2
AdanLink75 <141869449+AdanLink75@users.noreply.github.com>
2024-10-30 11:32:05 -0300

    h

--- perguntas.js
+++ perguntas.js
@@ -70,3 +70,87 @@ criaCartao(
     'Quantas visualizações o Enaldinho tem no video do caça leão? '
 )
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+criaCartao(
+    'matematica',
+    'como se calcula a soma dos termos de uma P.A'
+)
+
+criaCartao(
+    'matematica',
+    'quais são todos os numeros do pi existentes?'
+)
+
+criaCartao(
+    'matematica',
+    'se andrew usar 100g de maconha,  29 cigarros(20g) e e 2kg de cocaina, qual efeito daria no andrew? e qual a quantidade que ele usou?',
+)
+criaCartao(
+    'matematica',
+    'se um homem caisse do 13° de cabeça para baixo, quantos ossos iria quebrar e ligamentos iria romper?'
+)
+criaCartao(
+    'historia',
+    'após dos eventos da 2 guerra mundial, como ficou Adolf Hitler?'
+)
+criaCartao(
+    'historia',
+    'em qual ano o Lula(atua precidente do Brasil) roubou do Brasil?'
+)
+criaCartao(
+    'historia',
+    'quem era a esposa de charlie chaplin?'
+
+)
+criaCartao(
+    'histoia',
+    'quem foi Gilberto Freyre, e como ele impactou na vida de getulio vargas?'
+)
+criaCartao(
+    'portugues',
+    'para oque se usa o §?'
+)
+criaCartao(
+    'portugues',
+    'qual o livro que andrew mais gosta'
+)
+criaCartao(
+    'portugues',
+    'cite todos os livros, lugares e mulheres que já foram vivenciados por Machado de Assis'
+)
+criaCartao(
+    'portugues',
+    'qual o estilo de literatura que dj azeitona produz?'
+)
+criaCartao(
+    'arte',
+    'em qual episodio de "Valdir" Andrezitos reclamo sobre mendigos comunistas?'
+)
+criaCartao(
+    'arte',
+    'Em qual video do Gemaplys, Vito, fala sua iconica frase sobre japones?'
+)
+criaCartao(
+    'arte',
+    'como se foi montado a primeira bomba de hidrogenio?',
+)
+criaCartao(
+    'arte',
+    'para desenhar a monalisa, adan precisou usar 3 KG de crack, quantos ele precisaria para desenhar a turma de monica? e quantos o mauricio de souza utilizou? e quantos rabiscos tem a monica e por que? '
+)
+criaCartao(
+    'arte',
+    'Quantas visualizações o Enaldinho tem no video do caça leão? '
+)
