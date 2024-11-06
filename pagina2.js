f52d6b9b4d5a93aa1fccf6e7e285e823177c86e6
AdanLink75 <141869449+AdanLink75@users.noreply.github.com>
2024-11-06 12:01:18 -0300

    asd

--- pagina2.js
+++ pagina2.js
@@ -3,7 +3,7 @@ function criaCartao(categoria, pergunta, resposta) {
     let cartao = document.createElement('article')
     cartao.className = 'cartao';
     cartao.innerHTML = `
-    <div class="conteudo-cartao" onclink ="mostraResposta(this)">
+    <div class="conteudo-cartao" onclick ="mostraResposta(this)">
     <h3>${categoria}</h3>
     <div class="conteudo-pergunta">
         <p>${pergunta}</p>
