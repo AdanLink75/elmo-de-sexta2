3332c22d1f1f86f2e24d599ba36aa97fe7ef2ffe
AdanLink75 <141869449+AdanLink75@users.noreply.github.com>
2024-11-06 12:14:44 -0300

    asd

--- perguntas.js
+++ perguntas.js
@@ -1,75 +1,90 @@
 criaCartao(
     'matematica',
     'como se calcula a soma dos termos de uma P.A',
-    'sdcfvbnm,'
+    ' multiplicação da metade do seu número de termos pela soma do primeiro com o último termo.'
 )
 
 criaCartao(
     'matematica',
     'quais são todos os numeros do pi existentes?',
-    'sdfgbhnjm,.'
+    'vai saber.'
 )
 
 criaCartao(
     'matematica',
     'se andrew usar 100g de maconha,  29 cigarros(20g) e e 2kg de cocaina, qual efeito daria no andrew? e qual a quantidade que ele usou?',
+    'cai morto no chão. 2120g.'
 )
 criaCartao(
     'matematica',
-    'se um homem caisse do 13° de cabeça para baixo, quantos ossos iria quebrar e ligamentos iria romper?'
+    'se um homem caisse do 13° de cabeça para baixo, quantos ossos iria quebrar e ligamentos iria romper?',
+    '46 e 983'
 )
 criaCartao(
     'historia',
-    'após dos eventos da 2 guerra mundial, como ficou Adolf Hitler?'
+    'após dos eventos da 2 guerra mundial, como ficou Adolf Hitler?',
+    'vivo, mas morto'
 )
 criaCartao(
     'historia',
-    'em qual ano o Lula(atua precidente do Brasil) roubou do Brasil?'
+    'em qual ano o Lula(atua precidente do Brasil) roubou do Brasil?',
+    'todos'
 )
 criaCartao(
     'historia',
-    'quem era a esposa de charlie chaplin?'
+    'quem era a esposa de charlie chaplin?',
+    'John Cena'
 
 )
 criaCartao(
     'histoia',
-    'quem foi Gilberto Freyre, e como ele impactou na vida de getulio vargas?'
+    'quem foi Gilberto Freyre, e como ele impactou na vida de getulio vargas?',
+    'foi o irmão dele, e com muita ajuda, fundou a empresa SpaceX.'
 )
 criaCartao(
     'portugues',
-    'para oque se usa o §?'
+    'para oque se usa o §?',
+    'ninguém sabe'
 )
 criaCartao(
     'portugues',
-    'qual o livro que andrew mais gosta'
+    'qual o livro que andrew mais gosta',
+    'clube da luta'
 )
 criaCartao(
     'portugues',
-    'cite todos os livros, lugares e mulheres que já foram vivenciados por Machado de Assis'
+    'cite todos os livros, lugares e mulheres que já foram vivenciados por Machado de Assis',
+    'informções encontradas em https://www.friv.com/'
 )
 criaCartao(
     'portugues',
-    'qual o estilo de literatura que dj azeitona produz?'
+    'qual o estilo de literatura que dj azeitona produz?',
+    'rap cristão'
 )
 criaCartao(
     'arte',
-    'em qual episodio de "Valdir" Andrezitos reclamo sobre mendigos comunistas?'
+    'em qual episodio de "Valdir" Andrezitos reclamo sobre mendigos comunistas?',
+    'Todos, menos o episodio de como cirar um atômo de hidrogêneo'
 )
 criaCartao(
     'arte',
-    'Em qual video do Gemaplys, Vito, fala sua iconica frase sobre japones?'
+    'Em qual video do Gemaplys, Vito, fala sua iconica frase sobre japones?',
+    'O video em que participa do video de Resendete Eviel 2'
 )
 criaCartao(
     'arte',
     'como se foi montado a primeira bomba de hidrogenio?',
+    'após o episódio 34 de valdir "como criar um atômo de hidrogênio"'
 )
 criaCartao(
     'arte',
-    'para desenhar a monalisa, adan precisou usar 3 KG de crack, quantos ele precisaria para desenhar a turma de monica? e quantos o mauricio de souza utilizou? e quantos rabiscos tem a monica e por que? '
+    'para desenhar a monalisa, adan precisou usar 3 KG de crack, quantos ele precisaria para desenhar a turma de monica? e quantos o mauricio de souza utilizou? e quantos rabiscos tem a monica e por que? ',
+    'Maurício de Souza usou apenas muita criatividade e talento, sem precisar de nenhum "quilo" de crack. E a Mônica tem rabiscos porque ela adora se meter em confusão e sempre tem algo novo para contar'
 )
 criaCartao(
     'arte',
-    'Quantas visualizações o Enaldinho tem no video do caça leão? '
+    'Quantas visualizações o Enaldinho tem no video do caça leão? ',
+    '5 bilhões'
 )
 
 
