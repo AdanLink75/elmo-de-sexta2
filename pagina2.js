0b3be900d9b4dbfd2f756cc039e1619e18a9f1fe
AdanLink75 <141869449+AdanLink75@users.noreply.github.com>
2024-11-13 11:36:04 -0300

    asd

--- pagina2.js
+++ pagina2.js
@@ -1,29 +1,28 @@
-function criaCartao(categoria, pergunta, resposta) {
-    let container = document.getElementById('container')
-    let cartao = document.createElement('article')
+function criaCartao (categoria, pergunta, resposta){
+    let container = document.getElementById ('container');
+    let cartao = document.createElement('article');
     cartao.className = 'cartao';
     cartao.innerHTML = `
-    <div class="conteudo-cartao" onclick ="mostraResposta(this)">
-    <h3>${categoria}</h3>
-    <div class="conteudo-pergunta">
-        <p>${pergunta}</p>
-    </div>
-    <div class="conteudo-resposta">
-        <p>${resposta}</p>
-    </div>
-</div>
+    <div class="conteudo-cartao" onclick = "mostraResposta(this)">
+      <h3>${categoria}</h3>
+     <div class="conteudo-pergunta">
+      <p>${pergunta}</p>
+     </div>
+     <div class="conteudo-resposta">
+      <p>${resposta}</p>
+     </div>
+  </div>
     `
+  
+    container.appendChild(cartao);
+  }
 
-    container.appendChild(cartao)
-}
-
-function mostraResposta(card){
-    const resposta=card.querySelector('.conteudo-resposta');
+  function mostraResposta(card){
+    const resposta =card.querySelector('.conteudo-resposta');
     if(resposta.style.display === 'none'){
-        resposta.style.display = 'block';
+      resposta.style.display = 'block';
     }
     else{
-        resposta.style.display = 'none';
-
+       resposta.style.display = 'none';
     }
-}
+  }
